d9913b2c0aa39dc50287e56709f9a18c975fa2b8
Ahmeddhibi19 <adhibi345@gmail.com>
2024-11-15 23:29:36 +0100

    fixing jenkins file 14256956

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -52,8 +52,8 @@ pipeline {
                 script {
                     echo "Running Maven command: mvn clean package -Dspring.datasource.username=${env.MYSQL_USERNAME} -Dspring.datasource.password=${env.MYSQL_PASSWORD} -Djwt.secret=${env.JWT_SECRET} -Dspring.mail.username=${env.EMAIL_USERNAME} -Dspring.mail.password=${env.EMAIL_PASSWORD}"
                 }
-                //sh 'chmod +x ./mvnw'
-                sh "mvn clean package -Dspring.datasource.username=${env.MYSQL_USERNAME} -Dspring.datasource.password=${env.MYSQL_PASSWORD} -Djwt.secret=${env.JWT_SECRET} -Dspring.mail.username=${env.EMAIL_USERNAME} -Dspring.mail.password=${env.EMAIL_PASSWORD}"
+                sh 'chmod +x ./mvnw'
+                sh "./mvn clean package -Dspring.datasource.username=${env.MYSQL_USERNAME} -Dspring.datasource.password=${env.MYSQL_PASSWORD} -Djwt.secret=${env.JWT_SECRET} -Dspring.mail.username=${env.EMAIL_USERNAME} -Dspring.mail.password=${env.EMAIL_PASSWORD}"
             }
         }
 
