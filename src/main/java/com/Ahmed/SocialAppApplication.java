08a99be3738c8ad7ee26992f7d05e68258e15f69
Ahmeddhibi19 <adhibi345@gmail.com>
2024-11-16 00:34:22 +0100

    fixing jenkins file 26547

--- src/main/java/com/Ahmed/SocialAppApplication.java
+++ src/main/java/com/Ahmed/SocialAppApplication.java
@@ -2,11 +2,12 @@ package com.Ahmed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {BatchAutoConfiguration.class})
 @EnableJpaAuditing
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = "com.Ahmed.repository")
--- src/main/java/com/Ahmed/mapper/Mapper.java
+++ src/main/java/com/Ahmed/mapper/Mapper.java
@@ -4,9 +4,9 @@ import com.Ahmed.dto.UpdateUserInfoDto;
 import com.Ahmed.entity.User;
 import org.springframework.stereotype.Component;
 
-@Component
-public class Mapper implements MapstructMapperUpdate{
-    @Override
+
+public class Mapper {
+
     public void updateUserFromUserUpdateDto(UpdateUserInfoDto updateUserInfoDto, User user) {
 
     }
