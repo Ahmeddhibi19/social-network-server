f3f7245a3935eabbf4405cfddc9645668dcbb161
Ahmeddhibi19 <adhibi345@gmail.com>
2024-11-16 00:00:08 +0100

    fixing jenkins file 265

--- src/main/java/com/Ahmed/SocialAppApplication.java
+++ src/main/java/com/Ahmed/SocialAppApplication.java
@@ -2,8 +2,14 @@ package com.Ahmed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.Ahmed.repository")
 public class SocialAppApplication {
 
     public static void main(String[] args) {
