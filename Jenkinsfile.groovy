0c187546ef8df687d02f048d11f391dcb93f664f
Ahmeddhibi19 <adhibi345@gmail.com>
2024-11-16 00:17:07 +0100

    fixing jenkins file 2654

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -50,6 +50,7 @@ pipeline {
         stage('Build Spring Boot Application') {
             steps {
                 script {
+                    sh "docker logs mysql-container"
                     echo "Running Maven command: mvn clean package -Dspring.datasource.username=${env.MYSQL_USERNAME} -Dspring.datasource.password=${env.MYSQL_PASSWORD} -Djwt=${env.JWT_SECRET} -Dspring.mail.username=${env.EMAIL_USERNAME} -Dspring.mail.password=${env.EMAIL_PASSWORD}"
                     sh "mvn clean package"
                 }
